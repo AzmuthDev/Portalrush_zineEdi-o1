f06e8d8daa58822624f0201bff82dc744c732ccd
AzmuthDev <joseeduardorms29@gmail.com>
2026-07-09 01:03:01 -0300

    Implement Google Tag Manager

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import ReactGA from 'react-ga4'
+import TagManager from 'react-gtm-module'
 import './index.css'
 import App from './App.jsx'
 
 ReactGA.initialize('G-60HWSY02DV');
 ReactGA.send({ hitType: "pageview", page: window.location.pathname + window.location.search });
+
+const tagManagerArgs = {
+  gtmId: 'GTM-PWX2VGX7'
+};
+TagManager.initialize(tagManagerArgs);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
