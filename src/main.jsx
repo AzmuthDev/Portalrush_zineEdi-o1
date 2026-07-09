6c8864235029b85059675157ea5e465a678289dd
AzmuthDev <joseeduardorms29@gmail.com>
2026-07-08 21:40:11 -0300

    Implement GA4

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import ReactGA from 'react-ga4'
 import './index.css'
 import App from './App.jsx'
+
+ReactGA.initialize('G-4C7VZB2YGY');
+ReactGA.send({ hitType: "pageview", page: window.location.pathname + window.location.search });
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
